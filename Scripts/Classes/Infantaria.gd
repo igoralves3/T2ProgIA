5f233b2451a76d85a7c009cc051b2a571cb0e7ef
JoaoPioner <jvpioner191@gmail.com>
2025-06-15 19:50:53 -0300

    começo inimigo basico

--- Scripts/Classes/Infantaria.gd
+++ Scripts/Classes/Infantaria.gd
@@ -1,18 +1,19 @@
-extends Resource
+extends CharacterBody2D
 
 class_name Infantaria
 
 var vida: int
 var type: String
 var speed: int
+var pontos: int
 var fireRate: float
+var alvo: CharacterBody2D
 
 
-func _init(vida: int, type: String, speed: int, fireRate: float) -> void:
+func _init(vida: int, type: String, speed: int, fireRate: float, pontos: int, alvo: CharacterBody2D) -> void:
 	self.vida = vida
 	self.type = type
 	self.speed = speed
 	self.fireRate = fireRate
-	
-func movimentacao():
-	pass
+	self.pontos =  pontos
+	self.alvo = alvo
