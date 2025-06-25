145188f0012065f29b72be30fb30b0a5aa306131
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-06-24 22:02:09 -0300

    comecando a colocar o tiro do inimigo

--- Componentes/Inimigos/Infantaria/infantaria.gd
+++ Componentes/Inimigos/Infantaria/infantaria.gd
@@ -7,5 +7,32 @@ extends CharacterBody2D
 @export var fireRate: float
 @export var alvo: CharacterBody2D
 
+@export var bullet_inimigo: PackedScene
+
+@export var other_player: CharacterBody2D
+
+@export var timetoshoot: float = 0.0
+
+var curtimetoshoot : float = 0.0
+
+
+func updateTimer(delta: float):
+	curtimetoshoot = curtimetoshoot + delta
+	
+	if curtimetoshoot > timetoshoot:
+		curtimetoshoot = 0.0
+		timetoshoot = randf_range(10.0,20.0)
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+	
+	updateTimer(delta)
+	
+func _ready():
+	
+	timetoshoot = randf_range(10.0,20.0)
+	
+	if not other_player:
+		var currentScene = get_tree().get_current_scene().get_name()
+		other_player = get_node('/root/'+currentScene+'/MainPlayerChar')
+		
