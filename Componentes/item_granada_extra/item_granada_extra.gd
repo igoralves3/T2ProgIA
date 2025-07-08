6c86656f9f22c03526185ff61f2b16ad0b932f3c
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-08 19:52:37 -0300

    sprite para uma granada

--- Componentes/item_granada_extra/item_granada_extra.gd
+++ Componentes/item_granada_extra/item_granada_extra.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 class_name GranadaExtra
 
+@export var grenadeBonus = 1
+
+func _ready() -> void:
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		print('coletou granada')
-		body.grenadeAmmo =body.grenadeAmmo + 1
+		body.grenadeAmmo =body.grenadeAmmo + grenadeBonus
 		print('granadas: '+str(body.grenadeAmmo))
 		queue_free()
 	else:
