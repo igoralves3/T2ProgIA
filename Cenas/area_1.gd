bfcd822e373c246473d1fa112a969b36053b629f
João Vitor De Almeida Prado Pioner <Joao.Pioner@edu.pucrs.br>
2025-07-08 21:51:53 -0300

    inicio do checkpoint

--- Cenas/area_1.gd
+++ Cenas/area_1.gd
@@ -7,13 +7,19 @@ var posicao_camera: Vector2
 var camera_distancia_y_minima = 170
 var camera_altura_maxima_y = -1791.5
 
+var currentCheckpoint: Vector2
+@onready var startPoint: Vector2 = Vector2(74,100)
+
+func _ready() -> void:
+	GameManager.setStartPoint(startPoint)
+	%MainPlayerChar.global_position = GameManager.getSpawnPostion()
+
 func _process(delta: float) -> void:
 #	print (camera.offset.y, "offset")
 #	print (camera.position.y, "position")
 	if player.position.y - camera.offset.y < camera_distancia_y_minima and camera.offset.y >= camera_altura_maxima_y:
 		camera.offset.y = player.position.y -camera_distancia_y_minima
 	#player camera.offset 
-
 
 func _on_main_player_char_dead_player():
 	get_tree().reload_current_scene()
--- Componentes/Menu_etc/game_manager.gd
+++ Componentes/Menu_etc/game_manager.gd
@@ -1,1 +1,29 @@
 extends Node
+
+var checkPoint: Vector2
+var startPoint: Vector2
+var hasCheckpoint: bool
+var score: int
+var lifes: int
+var medals: int
+
+func newGame():
+	score = 0
+	lifes = 3
+	medals = 0
+	hasCheckpoint = false
+
+func setCheckPoint(newCheckPoint: Vector2):
+	self.checkPoint = newCheckPoint
+
+func setStartPoint(newStartPoint: Vector2):
+	self.startPoint = newStartPoint
+
+func addPoints(points: int):
+	score += points
+
+func getSpawnPostion():
+	if hasCheckpoint:
+		return checkPoint
+	else: 
+		return startPoint
