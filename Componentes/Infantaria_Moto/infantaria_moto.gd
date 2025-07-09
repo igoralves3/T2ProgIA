5c2e8934bbaeaaaa97c0c288abcf6d881cc9342c
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-08 21:47:58 -0300

    moto para até o jogador passar em baixo da ponte

--- Componentes/Infantaria_Moto/infantaria_moto.gd
+++ Componentes/Infantaria_Moto/infantaria_moto.gd
@@ -2,13 +2,33 @@ extends Area2D
 
 const SPEED := 50
 
+var parada = false
+var pode_parar = true
+
+
+
+var other_player : CharacterBody2D
 
 func _ready():
-	print('moto pronta')
+	if not other_player:
+		var currentScene = get_tree().get_current_scene().get_name()
+		other_player = get_node('/root/'+currentScene+'/MainPlayerChar')
+
 
 func _physics_process(delta: float) -> void:
-	print('moto dentro')
-	position.x = position.x -SPEED * delta
+	if parada == false:
+		position.x = position.x -SPEED * delta
+		if position.x >= other_player.position.x - 25 and position.x <= other_player.position.x + 25:
+			if pode_parar:
+				parada = true
+				pode_parar = false
+			
+	else:
+		
+		if position.y >= other_player.position.y:
+			parada = false
+		
+	
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
