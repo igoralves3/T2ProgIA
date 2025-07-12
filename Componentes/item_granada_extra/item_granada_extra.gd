e3bda6d9fcc9435f277399cdfc0ce545fd6e2030
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-12 18:06:44 -0300

    adicionando sprite as caixas de granadas

--- Componentes/item_granada_extra/item_granada_extra.gd
+++ Componentes/item_granada_extra/item_granada_extra.gd
@@ -4,8 +4,22 @@ class_name GranadaExtra
 
 @export var grenadeBonus = 1
 
+@export var grenade1_spr : Texture
+@export var grenade3_spr : Texture
+@export var grenade5_spr : Texture
+
+@onready var sprite = $Sprite2D
+
 func _ready() -> void:
-	pass
+	if grenadeBonus == 1:
+		sprite.texture = grenade1_spr
+	elif grenadeBonus == 3:
+		sprite.texture = grenade3_spr
+	elif grenadeBonus == 5:
+		sprite.texture = grenade5_spr
+	else:
+		grenadeBonus = 1
+		sprite.texture = grenade1_spr
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
