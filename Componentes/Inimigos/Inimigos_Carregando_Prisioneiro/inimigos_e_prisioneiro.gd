66341458ce57c2af2bff98df56970759dd6071f5
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-15 21:15:03 -0300

    nomes dos sons corrigidos. Som da morte do inimigo também

--- Componentes/Inimigos/Inimigos_Carregando_Prisioneiro/inimigos_e_prisioneiro.gd
+++ Componentes/Inimigos/Inimigos_Carregando_Prisioneiro/inimigos_e_prisioneiro.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-const SPEED:int = 100
+const SPEED:int = 50
 
 @export var inimigo_esq: CharacterBody2D
 @export var prisioneiro: CharacterBody2D
 @export var inimigo_dir: CharacterBody2D
 
 @export var other_player: CharacterBody2D
+
+var direction_side := Vector2(-0.7,-1)
 
 func _ready():
 	if not other_player:
@@ -17,11 +19,14 @@ func _physics_process(delta: float) -> void:
 	if inimigo_esq or inimigo_dir:
 		var distance = global_position - other_player.global_position
 		if distance.length() < 100:
+			position += direction_side
+			"""
 			position.y =position.y - SPEED * delta
 			if global_position.x < other_player.global_position.x:
 				position.x =position.x - SPEED * delta
 			elif global_position.x > other_player.global_position.x:
 				position.x =position.x + SPEED * delta
+			"""
 	elif not inimigo_esq and not inimigo_dir:
 		prisioneiro.preso = false
 			
