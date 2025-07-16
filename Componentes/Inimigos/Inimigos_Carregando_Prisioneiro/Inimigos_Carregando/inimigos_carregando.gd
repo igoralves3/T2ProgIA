43d66e9b092663432176611fe432ef75d95e3659
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-15 21:04:16 -0300

    som de morte no inimigo que leva prisioneiro

--- Componentes/Inimigos/Inimigos_Carregando_Prisioneiro/Inimigos_Carregando/inimigos_carregando.gd
+++ Componentes/Inimigos/Inimigos_Carregando_Prisioneiro/Inimigos_Carregando/inimigos_carregando.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 
 @onready var _animated_sprite:= $AnimatedSprite2D
 
+@export var som_morte = AudioStream
+
 signal dead_enemy(myself: CharacterBody2D)
 
 func _ready() -> void:
@@ -13,6 +15,7 @@ func _ready() -> void:
 func bullet_hit():
 	set_collision_layer_value(3, false)
 	$Area2DColisaoMorte.set_collision_layer_value(3, false)
-	SFXDeath.play()
+	#SFXDeath.play()
+	SoundController.play_button(som_morte)
 	dead_enemy.emit(self)	
 	queue_free()
