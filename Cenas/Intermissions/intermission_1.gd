e3ae50de92ca48e52f8a7e64d8ffb408f9330cb7
PelvicPsycho <39059827+PelvicPsycho@users.noreply.github.com>
2025-07-26 15:50:44 -0300

    intermission adicionado

---
+++ Cenas/Intermissions/intermission_1.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var timer_label: Timer
+@export var texto:= "BROKE THE 1ST AREA
+NOW RUSH TO THE 2ND AREA"
+@export var label: Label
+var qtd_de_letras: int = 0
+@export var tempo_timer: float = 0.06
+@export var som_texto = AudioStream
+@export var animacao: AnimatedSprite2D
+@export var timer_avancar_cena: Timer
+@export var tempo_avancar_cena: float = 7.0
+
+func _ready() -> void:
+	animacao.play()
+	label.text = ""
+	timer_label.wait_time = tempo_timer
+	timer_label.one_shot = false
+	timer_label.start()
+
+func _on_timer_timeout() -> void:
+	if qtd_de_letras < texto.length():
+		qtd_de_letras += 1
+		label.text = texto.substr(0, qtd_de_letras)
+		SoundMasterController.play_button(som_texto)
+
+func _on_timer_avancar_cena_timeout() -> void: ##avanca a cena aqui
+	pass # Replace with function body.
