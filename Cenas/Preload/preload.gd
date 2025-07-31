cdbfd0724ef2bacce8b6dc99043c90942cd6dbe8
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-07-31 18:36:31 -0300

    começando a fazer o texto de se preparar para fase 1

---
+++ Cenas/Preload/preload.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+@onready var player_label = $PlayerLabel
+@onready var ready_label = $ReadyLabel
+@onready var timer = $Timer
+
+var show_player_label = true
+
+func _ready():
+	position = GameManager.getSpawnPostion()
+	show_player_label=true
+	
+func _physics_process(delta: float) -> void:
+	if show_player_label:
+		player_label.visible = true
+	else:
+		player_label.visible = false
+		
+func _on_timer_timeout() -> void:
+	show_player_label = !show_player_label
